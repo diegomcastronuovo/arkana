ad4686c56314d7068c6df482301231a1ae7a75ec
Diego <diegomcastronuovo@gmail.com>
2025-12-10 12:52:27 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <header className="header">
+        <a href="/" className="logo-link">
+          <img src="/Logo.svg" alt="Arkana Logo" className="logo" />
+        </a>
+      </header>
+      <main className="main">
+        <h1>Bienvenido a Arkana</h1>
+        <p>Tu sitio web está listo.</p>
+      </main>
+    </div>
+  )
+}
+
+export default App
